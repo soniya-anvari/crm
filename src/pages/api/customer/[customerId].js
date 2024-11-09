02f1ab5bed3d17667efd2e8524f05cdeb1c5ab78
soniya <soniyaavdev@gmail.com>
2024-11-09 02:36:43 -0800

    development of customer editingr

---
+++ src/pages/api/customer/[customerId].js
@@ -0,0 +1,24 @@
+import Customer from "@/models/customer";
+import connectDB from "@/utils/connectDB";
+
+export default async function handler(req,res){
+    try{
+        await connectDB()
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message:"error to connecting to DB"})
+        return 
+    }
+    if (req.method == "GET"){
+        const id=req.body.customerId
+        try{
+            const customer=Customer.findOne({_id:id})
+            res.status(200).json({status:"success",data:customer})
+        }
+        catch(err){
+            res.status(500).json({status:"failed",message:"Error in Retrieving from DB"})
+        }
+    }
+    
+        
+}
